2e50a23fcc9dd4ca5e66fa7f342e63401ef15c60
name1e5s <name1e5s@qq.com>
2021-07-16 20:10:07 +0800

    SpinalHDL 1.6.0

--- src/main/scala/mylib/MyTopLevel.scala
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -26,9 +26,9 @@ import scala.util.Random
 //Hardware definition
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool
-    val cond1 = in  Bool
-    val flag  = out Bool
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
     val state = out UInt(8 bits)
   }
   val counter = Reg(UInt(8 bits)) init(0)
