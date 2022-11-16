09e7cac187b8bcf53566cc0b5848bf7c5a42dfbe
Côme ALLART <numero_744@net-c.com>
2022-11-16 21:16:23 +0100

    restore fancy formatting in io

--- src/main/scala/mylib/MyTopLevel.scala
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -5,10 +5,10 @@ import spinal.core._
 // Hardware definition
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in Bool ()
-    val cond1 = in Bool ()
-    val flag = out Bool ()
-    val state = out UInt (8 bits)
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
+    val state = out UInt(8 bits)
   }
 
   val counter = Reg(UInt(8 bits)) init 0
